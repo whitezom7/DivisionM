f3d9d28b24f5292475b4e8fcbe92769a17dafbb8
Taylor Whitewood <taylorwhitewood2022@gmail.com>
2023-01-01 17:08:22 +0000

    Cleared unnecessary files

--- resources/[qb]/qb-scoreboard/config.lua
+++ resources/[qb]/qb-scoreboard/config.lua
@@ -6,7 +6,7 @@ Config.OpenKey = 'HOME' -- https://docs.fivem.net/docs/game-references/input-map
 Config.Toggle = true -- If this is false the scoreboard stays open only when you hold the OpenKey button, if this is true the scoreboard will be openned and closed with the OpenKey button
 
 -- Max Server Players
-Config.MaxPlayers = GetConvarInt('sv_maxclients', 48) -- It returns 48 if it cant find the Convar Int
+Config.MaxPlayers = GetConvarInt('sv_maxclients', 64) -- It returns 48 if it cant find the Convar Int
 
 -- Minimum Police for Actions
 Config.IllegalActions = {
